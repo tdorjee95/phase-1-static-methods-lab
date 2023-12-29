42b1c071ccbbc836d716efdd12550adf466efc01
Tenzin Dorjee <tenzindorjee95@yahoo.com>
2023-12-29 01:35:09 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
-class Formatter {
-  //add static methods here
-}
+
+  class Formatter {
+    //add static methods here
+    static capitalize(word) {
+
+      word = word.charAt(0).toUpperCase() + word.slice(1)
+      return word
+    }
+  
+    static sanitize(word) {
+      const newWord = word.split(" ")
+      const newWordArr = []
+      for (word of newWord) {
+        word = word.replace(/[^A-Za-z0-9-']+/g, "")
+        newWordArr.push(word)
+      }
+      return newWordArr.join(" ")
+    }
+  
+    static titleize(word) {
+      const noTitleizeList = ["the","a", "an", "but", "of", "and", "for", "at", "by", "from"]
+      const newWordArr = []
+      word = word.charAt(0).toUpperCase() + word.slice(1)
+      const words = word.split(" ")
+      for (word of words) {
+        if(!(noTitleizeList.includes(word))) {
+          word = word.charAt(0).toUpperCase() + word.slice(1)
+          newWordArr.push(word)
+        }
+        else {
+          newWordArr.push(word)
+        }
+      }
+      return newWordArr.join(" ")
+    }
+  }
